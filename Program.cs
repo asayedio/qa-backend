0ebbb7a53c5c5c3882e09fa71887c0aa94c89d5f
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-03-05 18:30:18 +0200

    install and set up DbUp for database migration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using DbUp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+
+ConfigurationManager configuration = builder.Configuration;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // For managing migrations using DbUp
+    var connectionString = configuration.GetConnectionString("DefaultConnection");
+    EnsureDatabase.For.SqlDatabase(connectionString);
+    // Create and confgure an instance of the DbUp upgrader
+    var upgrader = DeployChanges.To.SqlDatabase(connectionString, null)
+        .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly
+        .GetExecutingAssembly())
+        .WithTransaction()
+        .Build();
+    // Do a database migration if there are any pending SQL Scripts
+    /* We are using the IsUpgradeRequired method in the DbUp upgrade to check whether there are any pending SQL Scripts, and using the PerformUpgrade method to do the actual migration **/
+    if (upgrader.IsUpgradeRequired())
+        upgrader.PerformUpgrade();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
